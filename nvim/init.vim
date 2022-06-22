b5e8f79a1ab536a1d61ec87bd2bbf6f017e73e8a
Carlos <grzegorz.karlik@gmail.com>
2022-06-22 08:16:38 +0200

    fix: update nvim and vscode after some time

--- nvim/init.vim
+++ nvim/init.vim
@@ -20,20 +20,24 @@ syntax on
 
 call plug#begin()
 
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/nvim-cmp'
 Plug 'vim-airline/vim-airline'
 Plug 'preservim/nerdtree'
 Plug 'ap/vim-css-color'
 Plug 'rafi/awesome-vim-colorschemes'
 Plug 'ryanoasis/vim-devicons'
 Plug 'terryma/vim-multiple-cursors'
-Plug 'neoclide/coc.nvim'
 Plug 'fatih/vim-go'
 Plug 'vim-airline/vim-airline-themes'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-telescope/telescope-fzy-native.nvim'
-Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'jiangmiao/auto-pairs'
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'tpope/vim-fugitive'
@@ -42,9 +46,12 @@ Plug 'OmniSharp/omnisharp-vim'
 Plug 'tpope/vim-commentary'
 Plug 'neovim/nvim-lspconfig'
 Plug 'glepnir/lspsaga.nvim'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
 
 call plug#end()
 
+set completeopt=menu,menuone,noselect
 set encoding=UTF-8
 set fileencoding=utf-8
 set termencoding=utf-8
@@ -52,15 +59,13 @@ set termencoding=utf-8
 nnoremap <leader>nt <cmd>:NERDTreeToggle<CR>
 nnoremap <leader>nf <cmd>:NERDTreeFocus<CR>
 
+nnoremap <leader>fd <cmd>Telescope diagnostics<cr>
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
 
-inoremap <silent><expr> <c-space> coc#refresh()
-
 autocmd FileType go nmap <Leader>i <Plug>(go-info)
-set completeopt=longest,menuone
 
 let g:airline_powerline_fonts = 1
 
@@ -74,7 +79,6 @@ let g:go_highlight_function_calls = 1
 let g:go_auto_type_info = 1
 
 let g:OmniSharp_highlighting = 0
-let g:coc_sources_disable_map = { 'cs': ['cs-1', 'cs-2', 'cs-3'] }
 
 let g:airline#extensions#tabline#enabled = 1
 
--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -4,10 +4,11 @@ vim.opt.listchars:append("space:⋅")
 vim.opt.termguicolors = true
 
 local configs = require("lspconfig/configs")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require("lspconfig").gopls.setup({
-	cmd = { "gopls", "serve" },
+    capabilities = capabilities,
+    cmd = { "gopls", "serve" },
 	settings = {
 		gopls = {
 			analyses = {
@@ -41,6 +42,7 @@ require('telescope').setup{
   defaults = {
 	file_sorter = require('telescope.sorters').get_fzy_sorter,
 	color_devicons = true,
+    file_ignore_patterns = {"bin", "obj", "build", "node_modules", "vendored", "vendor"},
 
 	file_previewer = require('telescope.previewers').vim_buffer_cat.new,
 
@@ -51,5 +53,41 @@ require('telescope').setup{
   pickers = {},
   extensions = {}
 }
+
+local cmp = require'cmp'
+
+cmp.setup({
+snippet = {
+    expand = function(args)
+    require('luasnip').lsp_expand(args.body)
+    end,
+},
+window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+}, {
+    { name = 'buffer' },
+})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+}, {
+    { name = 'buffer' },
+})
+})
 
 require("telescope").load_extension('fzy_native')
